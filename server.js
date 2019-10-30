879d39fb0a8b87c9078c5701e62c2d7903129791
Linh <linhgraphic@gmail.com>
2019-10-30 21:22:33 +0000

    second

--- server.js
+++ server.js
@@ -4,16 +4,17 @@ const express = require("express");
 // initalise the server app
 const app = express();
 
-
 // serve automatically all files in the './public' folder
 app.use(express.static("public"));
 
 // serve the index.html file when visiting the homepage
-app.get("/", (request, response) => response.sendFile(__dirname + "/index.html"));
-
+app.get("/", (request, response) =>
+  response.sendFile(__dirname + "/index.html")
+);
 
 // port to listen on
-const port = 3000;
+// const port = 3000;
+const port = process.env.PORT;
 
 // start the server on the given port
 app.listen(port, () => console.log(`server listening on port ${port}`));
